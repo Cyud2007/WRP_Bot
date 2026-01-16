e4b9c9d30f63d19535b8f52dc48fd0e412c80dc8
Semen Poiarkov <68701806+Cyud2007@users.noreply.github.com>
2026-01-16 00:00:19 +0000

    Update BanCommand to use English options and messages

--- src/main/java/com/code/commands/Admin/BanCommand.java
+++ src/main/java/com/code/commands/Admin/BanCommand.java
@@ -19,51 +19,51 @@ public class BanCommand extends Command {
 
     @Override
     public CommandData createCommand() {
-        return Commands.slash("ban", "Банит игрока на сервере и отправляет сообщение с причиной.")
-                .addOptions(new OptionData(OptionType.USER, "игрок", "Игрок, которого нужно забанить", true))
-                .addOptions(new OptionData(OptionType.STRING, "причина", "Причина бана", true));
+        return Commands.slash("ban", "Bans a player from the server and sends a message with the reason.")
+                .addOptions(new OptionData(OptionType.USER, "player", "The player to be banned", true))
+                .addOptions(new OptionData(OptionType.STRING, "reason", "Reason for ban", true));
     }
 
     @Override
     public void execute(@NotNull SlashCommandInteractionEvent event) {
-        Member memberToBan = event.getOption("игрок").getAsMember();
-        String reason = event.getOption("причина").getAsString();
+        Member memberToBan = event.getOption("player").getAsMember();
+        String reason = event.getOption("reason").getAsString();
 
         if (memberToBan == null) {
-            event.reply("Не удалось найти указанного игрока.").setEphemeral(true).queue();
+            event.reply("The specified player could not be found..").setEphemeral(true).queue();
             return;
         }
 
         Member moderator = event.getMember();
 
         if (!moderator.hasPermission(Permission.BAN_MEMBERS)) {
-            event.reply("У вас нет прав на бан участников.").setEphemeral(true).queue();
+            event.reply("You do not have the right to ban members..").setEphemeral(true).queue();
             return;
         }
 
-        // Создание embed сообщения для текстового канала
+        // Creating an embed message for a text channel
         EmbedBuilder embedBuilder = new EmbedBuilder();
-        embedBuilder.setTitle("Участник забанен");
+        embedBuilder.setTitle("Member banned");
         embedBuilder.setDescription(memberToBan.getEffectiveName() + " был забанен.");
-        embedBuilder.setColor(new Color(255, 0, 0)); // Красный цвет
+        embedBuilder.setColor(new Color(255, 0, 0)); // Red
         embedBuilder.addField("Модератор", moderator.getEffectiveName(), false);
         embedBuilder.addField("Причина", reason, false);
 
-        // Отправка сообщения в текстовый канал
+        // Sending a message to a text channel
         event.replyEmbeds(embedBuilder.build()).queue();
 
-        // Отправка личного сообщения забаненному участнику
+        // Sending a private message to a banned user
         EmbedBuilder privateMessage = new EmbedBuilder();
-        privateMessage.setTitle("Вы были забанены на сервере");
-        privateMessage.setDescription("Вас забанили на сервере " + event.getGuild().getName() + ".");
-        privateMessage.setColor(new Color(255, 0, 0)); // Красный цвет
-        privateMessage.addField("Причина", reason, false);
+        privateMessage.setTitle("You have been banned from the server.");
+        privateMessage.setDescription("You have been banned from the server " + event.getGuild().getName() + ".");
+        privateMessage.setColor(new Color(255, 0, 0)); // Red
+        privateMessage.addField("Reason", reason, false);
 
         memberToBan.getUser().openPrivateChannel()
                 .flatMap(channel -> channel.sendMessageEmbeds(privateMessage.build()))
                 .queue();
 
-        // Бан участника с использованием Guild#ban
+        // Ban a member using Guild#ban
         event.getGuild().ban(memberToBan, 1, TimeUnit.DAYS).reason(reason).queue();
     }
 }
